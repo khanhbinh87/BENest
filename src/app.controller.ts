8a1c1bb9ef57ceea403bc5fc97047fc911241f4c
khanhbinh87 <khanhbinh1187@gmail.com>
2024-05-01 01:30:20 +0700

    use jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/commo
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configServie: ConfigService,
+    private authService: AuthService
   ) {}
 
   @Get()
@@ -22,7 +24,7 @@ export class AppController {
   @Post('/login')
    handleLogin(@Request() req) {
     {
-      return req.user;
+      return this.authService.login(req.user);
     }
   }
 }
